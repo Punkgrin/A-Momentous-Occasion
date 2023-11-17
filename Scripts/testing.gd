61a7e6aaddc34d15b5610de61aba6280c44b7677
Punkgrin <radarandromeda@gmail.com>
2023-11-17 08:09:33 -0800

    Optimization and hacky solutions

--- Scripts/testing.gd
+++ Scripts/testing.gd
@@ -6,3 +6,6 @@ func _process(delta):
 	$Player/GameplayUI/Debug/ZVelocity.text = "Z Velocity: " + str($Player.linear_velocity.z)
 	$Player/GameplayUI/Debug/Friction.text = "Friction: " + str($Player.physics.friction)
 	$Player/GameplayUI/Debug/OnTheFloor.text = "On The Floor: " + str($Player.on_the_floor)
+	$Player/GameplayUI/Debug/LeftWall.text = "Left Wall: " + str($Player/PitchPivot/LeftWallCheck.is_colliding())
+	$Player/GameplayUI/Debug/RightWall.text = "Right Wall: " + str($Player/PitchPivot/RightWallCheck.is_colliding())
+	
